fce2ca2ee0aff536f695b5d6a34ddaa1599bdfd6
Serge <sergesk17@gmail.com>
2019-01-28 10:26:59 +0200

    err function

--- resources/js/components/LoginComponent.vue
+++ resources/js/components/LoginComponent.vue
@@ -28,14 +28,17 @@
         },
         methods: {
             login(){
-                var app = this
+                let app = this;
                 this.$auth.login({
                     params: {
                         email: app.email,
                         password: app.password
                     },
                     success: function () {},
-                    error: function () {},
+                    error: function () {
+                        // debugger;
+                            this.error = true;
+                    },
                     rememberMe: true,
                     redirect: '/dashboard',
                     fetchUser: true,
